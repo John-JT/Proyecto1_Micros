63097777255fed1254b6f177fda15fbbbba3d11e
dalbc <dalberthc@gmail.com>
2017-09-26 14:30:54 -0600

    Add files via upload

--- ruta_ctrl.v
+++ ruta_ctrl.v
@@ -39,18 +39,25 @@ module ruta_ctrl(
     parameter [5:0] add = 6'h20;
     parameter [5:0] addi = 6'h08;
     parameter [5:0] aand = 6'h24;
-    parameter [5:0] aandi = 6'h0c;
+    parameter [5:0] andi = 6'h0c;
     parameter [5:0] j = 6'h02;
     parameter [5:0] jr = 6'h18;       //No es el verdadero funct de MIPS, se cambia por ser igual al addi
     parameter [5:0] lw = 6'h23;
     parameter [5:0] nnor = 6'h27;
     parameter [5:0] oor = 6'h25;
-    parameter [5:0] oori = 6'h0d;
+    parameter [5:0] ori = 6'h0d;
     parameter [5:0] slt = 6'h2a;
     parameter [5:0] slti = 6'h0a;
     parameter [5:0] sh = 6'h29;
     parameter [5:0] sw = 6'h2b;
     parameter [5:0] sub = 6'h22;
+    //Operaciones de la ALU
+    parameter ADD = 3'b001;
+    parameter SUB = 3'b010;
+    parameter AND = 3'b011;
+    parameter OR = 3'b100;
+    parameter NOR = 3'b101;
+    parameter COMPARE = 3'b110;
     //Otros parámetros de interés
     parameter [5:0] tipoR = 6'h00;
     parameter activo = 1'b0;
@@ -59,6 +66,8 @@ module ruta_ctrl(
     parameter zeroext = 1'b1;
     parameter word = 1'b0;
     parameter halfword = 1'b1;
+    parameter rt = 1'b0;
+    parameter rd = 1'b1;
 
 
 
@@ -93,6 +102,7 @@ module ruta_ctrl(
         codigop = opcode;
     end
 
+    //IF
     //Selección de la próxima dirección de la memoria de intrucciones a ejecutar, para los jumps
     //se cambia el orden normal de ejecución (SEL_DIR = 00 = PC+4)
     always @(codigop)
@@ -107,16 +117,17 @@ module ruta_ctrl(
       endcase
     end
 
+    //ID
     //En el caso de las dos instrucciones de salto, se debe resetear el pipe IF/ID
     always @ (codigop)
     begin
       case (codigop)
         j:
-            resetIFr = activo;
+            resetIFr = 1'b1;
         jr:
-            resetIFr = activo;
+            resetIFr = 1'b1;
         default:
-            resetIFr = desactivo;
+            resetIFr = 1'b0;
       endcase
     end
 
@@ -135,7 +146,7 @@ module ruta_ctrl(
     // 0: sign, 1: zero.
     always @ (codigop)
     begin
-      case (condition)
+      case (codigop)
         addi:
             SEL_IMr = signext;
         andi:
@@ -148,10 +159,150 @@ module ruta_ctrl(
             SEL_IMr = signext;
         sh:
             SEL_IMr = signext;
-        sw:    
+        sw:
             SEL_IMr = signext;
         default:
-            SEL_IMr = zeroext;     //en realidad para los demás casos no importa (X) pero se pone un 1 como default
+            SEL_IMr = 1'b1;     //en realidad para los demás casos no importa (X) pero se pone un 1 como default
+      endcase
+    end
+
+    //EXE
+    //Lógica del control de la ALU
+    always @ (codigop)
+    begin
+      case (codigop)
+        add:
+            ALU_FUN = ADD;
+        addi:
+            ALU_FUN = ADD;
+        aand:
+            ALU_FUN = AND;
+        andi:
+            ALU_FUN = AND;
+        lw:
+            ALU_FUN = ADD;
+        nnor:
+            ALU_FUN = NOR;
+        oor:
+            ALU_FUN = OR;
+        ori:
+            ALU_FUN = OR;
+        slt:
+            ALU_FUN = COMPARE;
+        slti:
+            ALU_FUN = COMPARE;
+        sh:
+            ALU_FUN = ADD;
+        sw:
+            ALU_FUN = ADD;
+        sub:
+            ALU_FUN = SUB;
+        default:
+            ALU_FUN = 3'b000;     //acá realmente no importa (X) lo que haga la ALU, se llegó a convenio de mandar 0
+      endcase
+    end
+
+    //Lógica para elegir si la ALU opera sobre el immediate extendido o rt
+    always @ (codigop)
+    begin
+      case (codigop)
+        add:
+            SEL_ALU = 1'b0;
+        addi:
+            SEL_ALU = 1'b1;
+        aand:
+            SEL_ALU = 1'b0;
+        andi:
+            SEL_ALU = 1'b1;
+        lw:
+            SEL_ALU = 1'b1;
+        nnor:
+            SEL_ALU = 1'b0;
+        oor:
+            SEL_ALU = 1'b0;
+        ori:
+            SEL_ALU = 1'b1;
+        slt:
+            SEL_ALU = 1'b0;
+        slti:
+            SEL_ALU = 1'b1;
+        sh:
+            SEL_ALU = 1'b1;
+        sw:
+            SEL_ALU = 1'b1;
+        sub:
+            SEL_ALU = 1'b0;
+        default:
+            SEL_ALU = 1'b0;     //acá realmente no importa (X) con lo que opere la ALU, se llegó a convenio de mandar 0
+      endcase
+    end
+
+    //Selecciona si por el pipeline se envía rt o rd para la dirección de WB
+    always @ (codigop)
+    begin
+        case (codigop)
+          add:
+              SEL_REG = rd;
+          addi:
+              SEL_REG = rt;
+          aand:
+              SEL_REG = rd;
+          andi:
+              SEL_REG = rt;
+          lw:
+              SEL_REG = rt;
+          nnor:
+              SEL_REG = rd;
+          oor:
+              SEL_REG = rd;
+          ori:
+              SEL_REG = rt;
+          slt:
+              SEL_REG = rd;
+          slti:
+              SEL_REG = rt;
+          sub:
+              SEL_REG = rd;
+          default:
+              SEL_REG = 1'b0;     //acá realmente no importa (X) lo que el MUX pase, se llegó a convenio de mandar 0
+        endcase
+    end
+
+    //MEM
+    //Solo se lee de memoria cuando se va hacer un load word:
+    always @ (codigop)
+    begin
+      case (codigop)
+        lw:
+            MEM_RD = activo;
+        default:
+            MEM_RD = desactivo;
+      endcase
+    end
+
+    //Se escribe en memoria para storehalfword y storeword:
+    always @ (codigop)
+    begin
+      case (codigop)
+        sh:
+            MEM_WR = activo;
+        sw:
+            MEM_WR = activo;
+        default:
+            MEM_WR = desactivo;
+      endcase
+    end
+
+    //Bit que le dice a la memoria si guardar los 32bits del dato o los 16bits menos significativos
+    always @ (codigop)
+    begin
+      case (codigop)
+        sh:
+            w_h = halfword;
+        sw:
+            w_h = word;
+        default:
+            w_h = word;
       endcase
     end
 
--- ruta_ctrl_tb.v
+++ ruta_ctrl_tb.v
@@ -10,6 +10,7 @@
 
 module ruta_ctrl_tb(
     );
+    //Wires de interconexión
     wire [5:0] opcode;
     wire [5:0] funct;
     wire MEM_RD_I;
@@ -20,16 +21,27 @@ module ruta_ctrl_tb(
     wire [4:0] ctrl_EXE;
     wire [2:0] ctrl_MEM;
     wire [1:0] ctrl_WB;
-    wire [5:0] codigopw;
-
-    parameter [5:0] tipoR = 6'h00;
+    //Parámetros de interés
     parameter [5:0] add = 6'h20;
-    parameter [5:0] sub = 6'h22;
+    parameter [5:0] addi = 6'h08;
+    parameter [5:0] aand = 6'h24;
+    parameter [5:0] andi = 6'h0c;
+    parameter [5:0] j = 6'h02;
+    parameter [5:0] jr = 6'h18;
+    parameter [5:0] lw = 6'h23;
+    parameter [5:0] nnor = 6'h27;
+    parameter [5:0] oor = 6'h25;
+    parameter [5:0] ori = 6'h0d;
+    parameter [5:0] slt = 6'h2a;
     parameter [5:0] slti = 6'h0a;
+    parameter [5:0] sh = 6'h29;
+    parameter [5:0] sw = 6'h2b;
+    parameter [5:0] sub = 6'h22;
+    parameter [5:0] tipoR = 6'h00;
 
 
 
-    //---INSTANCIACION---
+    //---Instanciación---
     ruta_ctrl inst_ruta_ctrl(
           .opcode(opcode),
           .funct(funct),
@@ -40,34 +52,64 @@ module ruta_ctrl_tb(
           .SEL_IM(SEL_IM),
           .ctrl_EXE(ctrl_EXE),
           .ctrl_MEM(ctrl_MEM),
-          .ctrl_WB(ctrl_WB),
-          .codigopw(codigopw)
+          .ctrl_WB(ctrl_WB)
                              );
 
 
 
-    //---VARIABLES---
-    reg [5:0] opcodereg;
-    reg [5:0] functreg;
+    //---Regs de entrada---
+    reg [5:0] opcoder;
+    reg [5:0] functr;
 
 
 
-    //---INICIALIZACIONES---
+    //---Inicializaciones y cambios en el tiempo---
     initial
     begin
-    opcodereg <= tipoR;
-    functreg <= add;
+    opcoder <= tipoR;
+    functr <= add;
 
-    #500
-    opcodereg <= tipoR;
-    functreg <= sub;
-
-    #50
-    opcodereg <= slti;
+    #100
+    opcoder <= addi;
+    #100
+    opcoder <= tipoR;
+    functr <= aand;
+    #100
+    opcoder <= andi;
+    #100
+    opcoder <= j;
+    #100
+    opcoder <= tipoR;
+    functr <= 6'h08;
+    #100
+    opcoder <= lw;
+    #100
+    opcoder <= tipoR;
+    functr <= nnor;
+    #100
+    opcoder <= tipoR;
+    functr <= oor;
+    #100
+    opcoder <= ori;
+    #100
+    opcoder <= tipoR;
+    functr <= slt;
+    #100
+    opcoder <= slti;
+    #100
+    opcoder <= sh;
+    #100
+    opcoder <= sw;
+    #100
+    opcoder <= tipoR;
+    functr <= sub;
     end
 
-    assign opcode = opcodereg;
-    assign funct = functreg;
+
+
+    //---Asignacion de wires de interconexión---
+    assign opcode = opcoder;
+    assign funct = functr;
 
 
 
