84a1b78efcd61500e0463224c1c33ff2308eef55
dalbc <dalberthc@gmail.com>
2017-09-26 00:04:01 -0600

    Add files via upload

--- ruta_ctrl.v
+++ ruta_ctrl.v
@@ -36,7 +36,6 @@ module ruta_ctrl(
 
     //---Parámetros Importantes---
     //Instrucciones: Para el caso de los tipo R se parametriza el funct, para los demás el opcode
-    parameter [5:0] tipoR = 6'h00;
     parameter [5:0] add = 6'h20;
     parameter [5:0] addi = 6'h08;
     parameter [5:0] aand = 6'h24;
@@ -52,23 +51,31 @@ module ruta_ctrl(
     parameter [5:0] sh = 6'h29;
     parameter [5:0] sw = 6'h2b;
     parameter [5:0] sub = 6'h22;
+    //Otros parámetros de interés
+    parameter [5:0] tipoR = 6'h00;
+    parameter activo = 1'b0;
+    parameter desactivo = 1'b1;
+    parameter signext = 1'b0;
+    parameter zeroext = 1'b1;
+    parameter word = 1'b0;
+    parameter halfword = 1'b1;
 
 
 
     //---Definición e Inicialización de Variables Reg y Wire---
     reg [5:0] codigop = 6'h00;        // guarda el opcode o funct de la instrucción
     reg [1:0] SEL_DIRr = 2'b00;       // selecciona la próxima instrucción
-    reg resetIFr = 1'b0;              // resetea el pipe
-    reg REG_RDr = 1'b1;               // en 0 indica que se va leer del banco de reg
-    reg SEL_IMr = 1'b1;               // en 1 hace zero extension al imm, en 0 le hace sign extension
+    reg resetIFr = desactivo;         // resetea el pipe
+    reg REG_RDr = desactivo;          // indica si se va leer del banco de reg
+    reg SEL_IMr = zeroext;            // indica el tipo de extensión que se le hace al imm
     reg [2:0] ALU_FUN = 3'b000;       // elige la funcion de la ALU
-    reg SEL_ALU = 1'b0;               // selecciona si la ALU opera con rt o el imm
-    reg SEL_REG = 1'b0;               // selecciona entre mandar rt por el pipe o rd
-    reg MEM_RD = 1'b1;                // en 0 indica que se va leer de la memoria principal
-    reg MEM_WR = 1'b1;                // en 0 indica que se va escribir en la memoria
-    reg w_h = 1'b0;                   // en 0 se va escribir un word, en 1 un halfword
-    reg DIR_WB = 1'b1;                // seleccion de mux de lo que se va sobrescribir en el banco (1 es salida de ALU)
-    reg REG_WR = 1'b1;                // en 0 indica que se va escribir en el banco de reg
+    reg SEL_ALU = 1'b0;               // selecciona si la ALU opera con rt (0) o el imm (1)
+    reg SEL_REG = 1'b0;               // selecciona entre mandar rt (0) por el pipe o rd (1)
+    reg MEM_RD = desactivo;           // indica si se va leer de la memoria principal
+    reg MEM_WR = desactivo;           // indica si se va escribir en la memoria
+    reg w_h = word;                   // indica el tipo de dato que se va guardar en memoria
+    reg DIR_WB = 1'b1;                // selecciona lo que se va sobrescribir en el banco (1 es salida de ALU, 0 salida mem)
+    reg REG_WR = desactivo;           // indica si se va escribir en el banco de reg
 
 
 
@@ -105,11 +112,11 @@ module ruta_ctrl(
     begin
       case (codigop)
         j:
-            resetIFr = 1'b1;
+            resetIFr = activo;
         jr:
-            resetIFr = 1'b1;
+            resetIFr = activo;
         default:
-            resetIFr = 1'b0;
+            resetIFr = desactivo;
       endcase
     end
 
@@ -118,9 +125,33 @@ module ruta_ctrl(
     begin
       case (codigop)
         j:
-            REG_RDr = 1'b1;
+            REG_RDr = desactivo;
         default:
-            REG_RDr = 1'b0;
+            REG_RDr = activo;
+      endcase
+    end
+
+    //Definición de para cuáles instrucciones hacer sign-extension y para cuáles zero-extension
+    // 0: sign, 1: zero.
+    always @ (codigop)
+    begin
+      case (condition)
+        addi:
+            SEL_IMr = signext;
+        andi:
+            SEL_IMr = zeroext;
+        lw:
+            SEL_IMr = signext;
+        ori:
+            SEL_IMr = zeroext;
+        slti:
+            SEL_IMr = signext;
+        sh:
+            SEL_IMr = signext;
+        sw:    
+            SEL_IMr = signext;
+        default:
+            SEL_IMr = zeroext;     //en realidad para los demás casos no importa (X) pero se pone un 1 como default
       endcase
     end
 
